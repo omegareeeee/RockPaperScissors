9bf817d3f74e469fc5ef564c21bebb88fa2c2489
Teerark Sith <teerarksith@gmail.com>
2023-06-11 15:38:08 -0500

    add playRound function

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ function getComputerChoice (){
         return "scissor";
     }
 }
+ function playRound(playerChoice, computerChoice){
+    if (playerChoice = "rock"){
+        if (computerChoice = "paper"){
+            return "you lose"
+        }
+        if (computerChoice = "scissors"){
+            return "you won"
+        }
+        else{
+            return "you tied"
+        }
+    }  
+    if (playerChoice = "paper"){
+        if (computerChoice = "scissors"){
+            return "you lose"
+        }
+        if (computerChoice = "rock"){
+            return "you won"
+        }
+        else{
+            return "you tied"
+        }
+    }
+    if (playerChoice = "scissor"){
+        if (computerChoice = "rock"){
+            return "you lose"
+        }
+        if (computerChoice = "paper"){
+            return "you won"
+        }
+        else{
+            return "you tied"
+        }
+    }
+ }
