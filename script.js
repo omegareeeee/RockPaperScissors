aff4af2b3a78009118a3e871e4284cefc036e9b8
Teerark Sith <teerarksith@gmail.com>
2023-06-13 02:56:27 -0500

    stuff

--- script.js
+++ script.js
@@ -16,34 +16,43 @@ function getComputerChoice (){
     if (playerChoice == "rock"){
         if (computerChoice == "paper"){
             console.log( "You Lose! Paper beats Rock");
+            return false;
         }
         if (computerChoice == "scissor"){
             console.log( "You Won! Rock beats Scissors");
+            return true;
         }
         else{
             console.log( "You Tied!");
+            return null;
         }
     }  
     if (playerChoice == "paper"){
         if (computerChoice == "scissor"){
             console.log( "You Lose! Scissor beats Paper");
+            return false;
         }
         if (computerChoice == "rock"){
             console.log( "You Won! Paper beats Rock");
+            return true;
         }
         else{
             console.log( "You Tied!");
+            return null;
         }
     }
     if (playerChoice == "scissor"){
         if (computerChoice == "rock"){
             console.log( "You Lose! Rock beats Scissor");
+            return false;
         }
         if (computerChoice == "paper"){
             console.log( "You Won! Scissor beats Paper");
+            return true;
         }
         else{
             console.log( "You Tied");
+            return null;
         }
     }
 }
@@ -65,17 +74,56 @@ function getComputerChoice (){
 
     }
  }
+ function hasWinner(player, ai){
+    if(player == 5){
+        alert("You won!!");
+        return true;
+    }
 
+    if(ai == 5){
+        alert("You Lost!");
+        return true;
+    }
+ }
  const rock = document.querySelector('#rock');
  const paper = document.querySelector('#paper');
  const scissor = document.querySelector('#scissor');
+ const scoreBoard = document.querySelector('#scoreBoard');
+ let playerScore = 0;
+ let aiScore = 0;
 
  rock.addEventListener('click', () => {
-    playRound('rock');
+    result = playRound('rock');
+    if(result){
+        playerScore++;
+    } else if (result === false){
+        aiScore++;
+    }
+    scoreBoard.textContent = "ScoreBoard:" +playerScore + ":" + aiScore;
+    hasWinner(playerScore, aiScore);
  });
+
  paper.addEventListener('click', () => {
-    playRound('paper');
+    result = playRound('paper');
+    if(result){
+        playerScore++;
+    }else if (result === false){
+        aiScore++;
+    }
+    scoreBoard.textContent = "ScoreBoard:" +playerScore + ":" + aiScore;
+    hasWinner(playerScore, aiScore);
  });
  scissor.addEventListener('click', () => {
-    playRound('scissor');
+    result = playRound('scissor');
+    if(result){
+        playerScore++;
+    } else if (result === false) {
+        aiScore++;
+    }
+    scoreBoard.textContent = "ScoreBoard:" +playerScore + ":" + aiScore;
+    hasWinner(playerScore, aiScore);
  });
+ 
+ 
+
+
